9cad3e5d2f231cadce715bae1aa97b4f93e89d2f
maayany_h <maayan.hoffman@gmail.com>
2014-12-25 21:31:35 +0200

    Update Gui.java
    remove a warning

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 
 import controller.ControllerLogic;
 
-import java.awt.Component;
-import java.awt.Dialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Iterator;
